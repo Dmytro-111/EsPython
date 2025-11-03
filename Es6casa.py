2221dac47ca3a0c5ff6a93819d4db3f661486b02
Dmytro-111 <repetskiidmutro@gmail.com>
2025-11-03 11:14:02 +0100

    Update Es6casa.py

--- Es6casa.py
+++ Es6casa.py
@@ -1,10 +1,9 @@
-# Data una lista,
-# costruire un dizionario che registri la frequenza di ciascun carattere presente in essa
-lista = ['a', 'b', 'a', 'c', 'b', 'a', 'd']
-frequenze = {}
-for carattere in lista:
-    if carattere in frequenze:
-        frequenze[carattere] += 1
+li = ['a', 'b', 'd', 'a', 'd', 'c', 'a']
+frequenza = {}
+for lettera in li:
+    if lettera in frequenza:
+        frequenza[lettera] += 1
     else:
-        frequenze[carattere] = 1
-print(frequenze)
+        frequenza[lettera] = 1
+
+print(frequenza)

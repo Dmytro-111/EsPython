c9a66be0d4a706326543ce785fc11bd52dd07aad
Dmytro-111 <repetskiidmutro@gmail.com>
2025-11-17 12:26:23 +0100

    Update Esercizio2_casa.py

--- Esercizio2_casa.py
+++ Esercizio2_casa.py
@@ -5,6 +5,6 @@ uguali = 0
 for var in li_1:
     if var in li_2:
         uguali += 1
-        print("Il numero comune e' :", elemento)
+        print("Il numero comune e' :", var)
 
 print("Il totale dei numeri uguali nelle due liste e' :", uguali)
